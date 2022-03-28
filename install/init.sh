7952baf77a8fefc87dedfb5dc71c010453e413ad
vik1497 <vik1497@localhost>
2022-03-28 15:03:27 -0600

    changed init script

--- install/init.sh
+++ install/init.sh
@@ -1,12 +1,12 @@
 # Run the following on the scheduler
 # first copy cloudlab.pem to the scheduler
-chmod 400 cloudlab.pem
-ssh-keygen -p -N "" -f ./cloudlab.pem
+#chmod 400 cloudlab.pem
+#ssh-keygen -p -N "" -f ./cloudlab.pem
 # enter the passphrase
 
 # Install pip and dask
-apt update
-apt install python3-pip
+sudo apt update
+sudo apt install python3-pip
 pip install "dask[complete]"
 pip install jupyter-server-proxy
 pip install notebook
@@ -16,10 +16,10 @@ echo "export PATH=$PATH:$HOME/.local/bin" >> ~/.bashrc
 source ~/.bashrc
 
 # copy .pem file to each of the workers
-scp -i cloudlab.pem cloudlab.pem <workerip>:/users/<username>
+#scp -i cloudlab.pem cloudlab.pem <workerip>:/users/<username>
 
 # Run the following on each of the workers
-apt update
-apt install -y python3-pip
+sudo apt update
+sudo apt install -y python3-pip
 pip install fabric
-chmod 400 cloudlab.pem
+#chmod 400 cloudlab.pem
