3747c9dcf2d10feb4695690cf78b1ab115f30f06
prsridha <prsridha@localhost>
2022-04-10 03:30:37 -0600

    debugging install script wip

--- install/utilities.py
+++ install/utilities.py
@@ -11,58 +11,82 @@ def import_or_install(package):
         import pip
         pip.main(['install', package])
 
+def init_fabric(w):
+    global s
+    global conn
+    global username
+
+    from fabric2 import ThreadingGroup, Connection
+
+    workers = ["node"+str(i) for i in range(1, w+1)]
+    host = "node0"
+    username = subprocess.run("whoami", capture_output=True, text=True).stdout.strip("\n")
+
+    #initialize fabric
+    user = username
+    pem_path = "/users/{}/cloudlab.pem".format(username)
+    connect_kwargs = {"key_filename": pem_path}
+    conn = Connection(host, user=user, connect_kwargs=connect_kwargs)
+    s = ThreadingGroup(*workers, user=user, connect_kwargs=connect_kwargs)
+
 
 def init(w):
     global conn
     global s
+    global username
 
     subprocess.call(["sudo", "apt", "update"])
     subprocess.call(["sudo", "apt", "install", "-y", "python3-pip"])
+    username = subprocess.run("whoami", capture_output=True, text=True).stdout.strip("\n")
     
     import_or_install("fabric2")
     import_or_install("dask[complete]")
-    subprocess.call(["bash", "/users/vik1497/etl-wip/install/path.sh"])
-    from fabric2 import SerialGroup, Connection
+    subprocess.call(["bash", "/users/{}/etl-wip/install/path.sh".format(username)])
 
-    workers = ["node"+str(i) for i in range(w)]
-    host = "node0"
-
-    #initialize fabric
-    user = "vik1497"
-    pem_path = "/users/vik1497/cloudlab.pem"
-    connect_kwargs = {"key_filename": pem_path}
-    conn = Connection(host, user=user, connect_kwargs=connect_kwargs)
-    s = SerialGroup(*workers, user=user, connect_kwargs=connect_kwargs)
-
-    s.run("rm -rf /users/vik1497/etl-wip")
+    init_fabric(w)
+    
+    s.run("rm -rf /users/{}/etl-wip".format(username))
     s.run("git clone https://github.com/prsridha/etl-wip.git")
 # copy cloudlab.pem to workers
 
 
 def copy_pem():
-    result = s.put("/users/vik1497/cloudlab.pem", "/users/vik1497")
+    global s
+    global username
+
+    result = s.put("/users/{}/cloudlab.pem".format(username), "/users/{}".format(username))
     return result
 
 
 def copy_module():
+    global s
+    global conn
+    global username
+
     print("Zipping cerebro")
-    conn.run("zip /users/vik1497/etl-wip/cerebro.zip /users/vik1497/etl-wip/cerebro/*")
-    s.run("mkdir -p /users/vik1497/etl-wip/etl-wip")
-    s.put("/users/vik1497/etl-wip/cerebro.zip",
-          "/users/vik1497/etl-wip/cerebro.zip")
-    s.put("/users/vik1497/etl-wip/requirements.txt",
-          "/users/vik1497/etl-wip/requirements.txt")
-    s.put("/users/vik1497/etl-wip/setup.py", "/users/vik1497/etl-wip/setup.py")
-    print(s.run("unzip /users/vik1497/etl-wip/cerebro.zip"))
-    print(s.run("cd /users/vik1497/etl-wip && python3 setup.py install --user"))
+    conn.run("zip /users/{}/etl-wip/cerebro.zip /users/{}/etl-wip/cerebro/*".format(username, username))
+    s.run("mkdir -p /users/{}/etl-wip/etl-wip".format(username))
+    s.put("/users/{}/etl-wip/cerebro.zip".format(username),
+          "/users/{}/etl-wip/cerebro.zip".format(username))
+    s.put("/users/{}/etl-wip/requirements.txt".format(username),
+          "/users/{}/etl-wip/requirements.txt".format(username))
+    s.put("/users/{}/etl-wip/setup.py".format(username), "/users/{}/etl-wip/setup.py".format(username))
+    print(s.run("unzip /users/{}/etl-wip/cerebro.zip".format(username)))
+    print(s.run("cd /users/{}/etl-wip && python3 setup.py install --user".format(username)))
 
 
 def run(cmd):
+    global s
+
     result = s.run(cmd)
     print(result)
 
 
 def start_dask():
+    global s
+    global conn
+    global username
+
     print("Starting Dask Scheduler")
     conn.run("dask-scheduler --host=0.0.0.0")
     print("Starting Dask workers")
@@ -70,11 +94,16 @@ def start_dask():
 
 
 def install_dependencies():
+    global s
+    global conn
+    global username
+
     print("Installing dependencies...")
 
     conn.sudo("sudo apt update")
     conn.sudo("sudo apt install -y python3-pip")
-    conn.run("pip install -r /users/vik1497/etl-wip/requirements.txt")
+    conn.sudo("sudo apt install -y python3-distributed")
+    conn.run("pip install -r /users/{}/etl-wip/requirements.txt".format(username))
 
     print("chmod /mydata/")
     conn.sudo("sudo chmod 777 -R /mydata/")
@@ -82,7 +111,7 @@ def install_dependencies():
     conn.sudo("sudo apt install dtach")
 
     print("Adding bin dir to path")
-    # conn.run("bash /users/vik1497/etl-wip/install/path.sh")
+    conn.run("bash /users/{}/etl-wip/install/path.sh".format(username))
 
     print("Installing more dependencies")
     conn.run("pip install click==7.1.1")
@@ -90,7 +119,8 @@ def install_dependencies():
     print("Installing dependencies on workers...")
     s.sudo("sudo apt update")
     s.sudo("sudo apt install -y python3-pip")
-    run("pip install -r /users/vik1497/etl-wip/requirements.txt")
+    s.sudo("sudo apt install -y python3-distributed")
+    run("pip install -r /users/{}/etl-wip/requirements.txt".format(username))
 
     print("chmod /mydata/")
     s.sudo("sudo chmod 777 -R /mydata/")
@@ -98,14 +128,18 @@ def install_dependencies():
     s.sudo("sudo apt install dtach")
 
     print("Adding bin dir to path")
-    s.put("/users/vik1497/etl-wip/install/path.sh", "/users/vik1497/")
-    s.run("bash /users/vik1497/path.sh")
+    s.put("/users/{}/etl-wip/install/path.sh".format(username), "/users/{}/".format(username))
+    s.run("bash /users/{}/path.sh".format(username))
 
     print("Installing more dependencies")
     run("pip install click==7.1.1")
 
 
 def delete_coco():
+    global s
+    global conn
+    global username
+
     print("Deleting /mydata/coco")
     s.run("rm -rf /mydata/coco/*")
 
@@ -115,22 +149,33 @@ def runbg(pre, cmd, sockname="dtach"):
 
 
 def testing():
-    # s.put("/users/vik1497/etl-wip/path.sh", "/users/vik1497")
-    conn.run("bash /users/vik1497/etl-wip/path.sh")
+    global s
+    global conn
+    global username
+
+    # s.put("/users/prsridha/etl-wip/path.sh", "/users/prsridha")
+    conn.run("bash /users/prsridha/etl-wip/path.sh")
 
 
 def setup_dask():
+    global s
+    global conn
+    global username
+
     from distributed.utils import get_ip
-    cmd1 = 'nohup bash -c "/users/vik1497/.local/bin/dask-scheduler --host=0.0.0.0"'
-    cmd2 = 'nohup bash -c "/users/vik1497/.local/bin/dask-worker tcp://{}:8786"'
+    cmd1 = 'nohup bash -c "/users/{}/.local/bin/dask-scheduler --host=0.0.0.0"'.format(username)
+    cmd2 = 'nohup bash -c "/users/{}/.local/bin/dask-worker tcp://{}:8786"'.format(username, get_ip())
     runbg(conn, cmd1)
-    runbg(s, cmd2.format(get_ip()))
+    runbg(s, cmd2)
     print("Done")
 
-
 def kill_dask():
-    user = "vik1497"
-    pem_path = "/users/vik1497/cloudlab.pem"
+    global s
+    global conn
+    global username
+
+    user = username
+    pem_path = "/users/{}/cloudlab.pem".format(username)
     connect_kwargs = {"key_filename": pem_path}
     from fabric2 import SerialGroup, Connection
 
@@ -160,23 +205,25 @@ def main():
                         help="number of workers")
 
     args = parser.parse_args()
-    init(args.workers)
 
     if args.cmd == "install":
+        init(args.workers)
         install_dependencies()
-    elif args.cmd == "testing":
-        testing()
-    elif args.cmd == "copypem":
-        out = copy_pem(s)
-        print(out)
-    elif args.cmd == "copymodule":
-        copy_module()
-    elif args.cmd == "deletecoco":
-        delete_coco()
-    elif args.cmd == "setupdask":
-        setup_dask()
-    elif args.cmd == "killdask":
-        kill_dask()
+    else:
+        init_fabric(args.workers)
+        if args.cmd == "testing":
+            testing()
+        elif args.cmd == "copypem":
+            out = copy_pem()
+            print(out)
+        elif args.cmd == "copymodule":
+            copy_module()
+        elif args.cmd == "deletecoco":
+            delete_coco()
+        elif args.cmd == "setupdask":
+            setup_dask()
+        elif args.cmd == "killdask":
+            kill_dask()
 
     conn.close()
     s.close()
