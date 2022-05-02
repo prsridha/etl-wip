f095858e18d7a50ba28a20dc3b4428d3ce117f7e
prsridha <prsridha@localhost>
2022-05-01 22:18:45 -0600

    added cocod download sh

--- install/utilities.sh
+++ install/utilities.sh
@@ -1,13 +1,3 @@
-dask-scheduler --host=0.0.0.0
-
-k get svc cerebro-controller-....
-
-dask-worker tcp://<>:8786
-
-
-
-
-
 cd /data/cerebro_data_storage
 wget http://images.cocodataset.org/zips/val2014.zip
 wget http://images.cocodataset.org/annotations/annotations_trainval2014.zip
