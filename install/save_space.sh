b3bdaf8df44253c5fbc8fff901d6c3a52b13ef8c
Pradyumna <prsridha@ucsd.edu>
2022-05-02 18:22:09 -0600

    added save space

---
+++ install/save_space.sh
@@ -0,0 +1,24 @@
+migrate_a2b (){
+    dir_a="$1"
+    dir_b="$2"
+    sudo mkdir -p $dir_b
+    sudo rsync -avr $dir_a/ $dir_b/
+    sudo rm -rvf $dir_a/*
+    sudo mount -o bind $dir_b/ $dir_a/
+    echo "$dir_b/    $dir_a/    none    bind    0    0" | sudo tee -a /etc/fstab
+}
+
+space_saver (){
+    ori_dir=$1
+    new_dir=$2
+    dir_b="${MNT_ROOT}/$new_dir"
+    dir_a="$ori_dir"
+    migrate_a2b $dir_a $dir_b
+
+}
+
+space_saver "/home" "home"
+space_saver "/tmp" "tmp"
+sudo chmod 1777 /tmp
+space_saver "/var" "var"
+sudo dpkg --configure -a
